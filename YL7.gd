d8e5903e9f0832795eb72e6fddf85d5250b1a37a
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-04-18 11:28:31 +0300

    Add files via upload

---
+++ YL7.gd
@@ -0,0 +1,19 @@
+extends Node
+
+
+func _ready():
+	pass 
+
+func _process(delta):
+	var padrunid = $"Node".padrunid
+	$"Bg2/ammo".text = "Ammo: "+ str(padrunid)
+	var score = $"Node".score
+	$"Bg2/score".text = "Score: "+ str(score)
+	var hp = $"Node".hp
+	$"Bg2/hp".text = "HP: "+ str(hp)
+	
+	if hp <= 0:
+		$"Bg2/Gameover".text = "GAME OVER!"
+		$"Bg2/ammo".text = ""
+		$"Bg2/score".text = ""
+		$"Bg2/hp".text = "HP: "+ str(hp)
---
+++ YL8.gd
@@ -0,0 +1,21 @@
+extends Node
+
+func _ready():
+	pass
+func _process(delta):
+	pass
+
+func _on_KIVI_pressed():
+	print("KIVI")
+
+
+func _on_PABER_pressed():
+	print("PABER")
+
+
+func _on_UUSMANG_pressed():
+	print("UUSMANG")
+
+
+func _on_KAARID_pressed():
+	print("KAARID")
