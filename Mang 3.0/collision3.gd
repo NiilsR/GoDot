3556d163ee42067f1757d50dcc1b46114bb75715
NiilsR <98737083+NiilsR@users.noreply.github.com>
2022-06-07 13:50:33 +0300

    Delete Mang 3.0 directory

--- Mang 3.0/collision3.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-func _physics_process(_delta):
-	var objs = get_overlapping_bodies()
-	for obj in objs:
-		if obj.name=="Player":
-			get_tree().change_scene('res://Menu/Menu.tscn')
